d4d0c7268968a09045ce8b783148ec288c9c4576
avinash-malviya <avinashmalviya500@gmail.com>
2021-06-26 23:53:59 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import MainComponent from './components/MainComponent';
+
+function App() {
+  return (
+    <div >
+      <header className="App-header">
+        
+        <p>
+          <MainComponent/>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
